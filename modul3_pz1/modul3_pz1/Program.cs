44238afc23bcc1058ef55a9589362ec47dd2951d
denys-u <denys.udovik@nure.ua>
2021-03-17 20:35:45 +0200

    created project and reposit

---
+++ modul3_pz1/modul3_pz1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace modul3_pz1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
